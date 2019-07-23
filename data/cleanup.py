2b00338219239a728f0a6d3c1186a572e40e5401
Fraser Price <fraserbcprice@gmail.com>
2019-07-23 16:02:35 +0100

    Modifing csv files and the csv cleaner

--- data/cleanup.py
+++ data/cleanup.py
@@ -2,7 +2,6 @@ csv_filenames = [
 	"carbonmonoxide.csv",
 	"nitricoxide.csv",
 	"nitrogendioxide.csv",
-	"Nitrogenoxidesasnitrogendioxide.csv",
 	"Non-volatile PM10-Hourly measured.csv",
 	"Non-volatile PM2_5 -Hourly measured.csv",
 	"Ozone.csv",
@@ -13,10 +12,14 @@ csv_filenames = [
 	"Volatile-PM2_5-Hourly-measured.csv"
 ]
 
+#Go through each files
 for name in csv_filenames:
 	with open(name, "r") as csv_file:
+		#Read all the lines
 		lines = csv_file.readlines()
 
+		#If it has a comma in it then it is valid csv and write it to a file
+		#This is a dirty way to do it but the extra text doesn't have any commas in it
 		with open("clean/{}".format(name), "w+") as out_clean:
 			for line in lines:
 				if "," in line:
