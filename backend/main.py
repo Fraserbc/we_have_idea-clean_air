f704c6c8efebaf2e1a107642a267265607e72b1c
Fraser Price <fraserbcprice@gmail.com>
2019-07-24 16:08:50 +0100

    COMMENTS NEED TO HAVE #  NOT JUST #

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
-#Import Flask and Flask Restful for the api
+# Import Flask and Flask Restful for the api
 from flask import Flask, request
 from flask_restful import Resource, Api
 import sqlite3, uuid, statistics, math
 
-#A nice decorator so I can use "@endpoint("/foo")"
+# A nice decorator so I can use "@endpoint("/foo")"
 def endpoint(endpoint):
 	def wrapper(callback):
 		class endpoint_class(Resource):
@@ -16,16 +16,16 @@ def endpoint(endpoint):
 	
 	return wrapper
 
-#Create the app and api
+# Create the app and api
 app = Flask(__name__)
 api = Api(app)
 
-#Just an echo endpoint for testing
+# Just an echo endpoint for testing
 @endpoint("/echo")
 def ping(json_data):
 	return {'echo': json_data}
 
-#The endpoint that takes lat long data and returns a score
+# The endpoint that takes lat long data and returns a score
 @endpoint("/score")
 def score(json_data):
 	db = sqlite3.connect("air_pollution.db")
@@ -33,7 +33,7 @@ def score(json_data):
 	cur.execute("SELECT location,lat,lon FROM data")
 	data = cur.fetchall()
 
-	#Lambda for sorting the list
+	# Lambda for sorting the list
 	sort = lambda x: (x[1]-json_data["lat"], x[2]-json_data["lon"])
 
 	data.sort(key=sort)
@@ -53,29 +53,29 @@ def score(json_data):
 				WHERE location = \"{}\"
 			"""
 	
-	#Loop through to two closest locations and get the average for all the points and get the distance
+	# Loop through to two closest locations and get the average for all the points and get the distance
 	average, distances = [], []
 	for location in data[:2]:
-		#Pythagorean theorem magic
+		# Pythagorean theorem magic
 		distances.append(abs( math.sqrt( abs(location[1]-json_data["lat"])**2 + abs(location[2]-json_data["lon"])**2 ) - 1))
 
-		#Get the data from the db
+		# Get the data from the db
 		cur.execute(query.format(location[0]))
 		data_points = cur.fetchall()[0]
 
-		#Get the average of the data points
+		# Get the average of the data points
 		average.append(statistics.mean(data_points))
 	
-	#Calculate the scores by making the numbers between 0 and 1 and multiplying them by the distance that has been f
-	#Flip them so 1 = 0 and 0 = 1 by taking away 1 and then using abs (absolute value)
-	#Multiply by 1000 so the numbers are between 0 and 1000
-	#Get the mean on those two numbers
-	#Round to the nearest integer
+	# Calculate the scores by making the numbers between 0 and 1 and multiplying them by the distance that has been f
+	# Flip them so 1 = 0 and 0 = 1 by taking away 1 and then using abs (absolute value)
+	# Multiply by 1000 so the numbers are between 0 and 1000
+	# Get the mean on those two numbers
+	# Round to the nearest integer
 	score = round(statistics.mean([abs(((x*y)/10)-1)*1000 for x, y in zip(average, distances)]))
 
-	#Return the score
+	# Return the score
 	return {'score':score}
 
-#Start the app
+# Start the app
 if __name__ == '__main__':
 	app.run()
